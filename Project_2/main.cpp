11b69c70ed1eb53d24fe770562d74d4cdeea7a78
ranewu <rane.hr.wu@gmail.com>
2013-03-13 20:30:54 -0700

    Finishing up the parsing function
    Wrapped up the parsing function. The component should work fine now.

--- Project_2/main.cpp
+++ Project_2/main.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 
 #define MAX_CPU_BURST 100
-#define MAX_IO_BURST 100
+//#define MAX_IO_BURST 100
 #define MAX_PROCESS 100
 
 struct process {
-  int PID;
+	int PID;
 	int TARQ;
 	int PRIO;
 	int TNCPU;
-	int TNIO;
+	//int TNIO;
 	int CPU[MAX_CPU_BURST];
-	int IO[MAX_IO_BURST];
+	int IO[MAX_CPU_BURST-1];
 } all_process[MAX_PROCESS];
 
 void myfunc() {
@@ -39,12 +39,24 @@ int string_spliter(string line, int *p_attributes, string delimiter){
 
 void string_parser(string line, process *out_process, int p_index){
 	int total_att;
-	int att[MAX_CPU_BURST+MAX_IO_BURST+4];
+	int att[MAX_CPU_BURST*2+3];
+	int cpu_c = 0;
+	int io_c = 0;
 	total_att = string_spliter(line, att, " ");
 	out_process[p_index].PID = att[0];
 	out_process[p_index].TARQ = att[1];
 	out_process[p_index].PRIO = att[2];
 	out_process[p_index].TNCPU = att[3];
+	for (int i = 4; i < total_att; i++){
+		if ((i%2) == 0){ 
+			out_process[p_index].CPU[cpu_c] = att[i];
+			cpu_c++;
+		}
+		else { 
+			out_process[p_index].IO[io_c] = att[i];
+			io_c++;
+		}
+	}
 	//out_process[p_index].TNCPU = att[3];
 }
 
@@ -66,10 +78,14 @@ int main(){
 	//myfunc();
 	myfile.close();
 	for (int i = 0; i< line_counter; i++){
-		cout <<all_process[i].PID<<" ";
+		cout <<"Process: "<<all_process[i].PID<<" ";
 		cout <<all_process[i].TARQ<<" ";
 		cout <<all_process[i].PRIO<<" ";
-		cout <<all_process[i].TNCPU<<" ";
+		cout <<all_process[i].TNCPU<<endl;
+		cout <<"CPU/IO Burst: ";
+		for (int j=0; j<all_process[i].TNCPU; j++){
+			cout <<all_process[i].CPU[j]<<" "<<all_process[i].IO[j]<<" ";
+		}
 		cout <<endl;
 	}
 	cout <<"Press enter to exit"<<endl;
