1900aec0ac32fde9987571df7cd99e76a8080c9d
Corsus <wtk.tommy@gmail.com>
2013-03-14 02:33:27 -0700

    Refactored and added functionality
    Enter to terminate implemented, Prompt user for file. Tests have been
    commented out.

--- Project_2/main.cpp
+++ Project_2/main.cpp
@@ -17,7 +17,12 @@ struct process_t {
 } all_process[MAX_PROCESS];
 
 typedef bool (*schedule_t) (process_t *process_list, int process_c);
+int string_spliter(string, int*, string);
+process_t string_parser(string);
 
+/*
+//Algorithm Template
+//=============================================================
 bool schedule1 (process_t *process_list, int process_c){
 	process_t process_local[MAX_PROCESS];
 	memcpy (process_local, process_list, MAX_PROCESS);
@@ -34,6 +39,56 @@ bool schedule2 (process_t *process_list, int process_c){
 	cout <<"test1"<<process_local[0].PID<<endl;
 	
 	return true;
+}
+//=============================================================
+*/
+
+int main(){ 
+	ifstream myfile;
+	string line;
+	string path;
+	cout << "Please enter the absolute path of the file: ";
+	cin >> path;
+	myfile.open(path);
+	//myfile<<"Writing this to a file..... \n";
+	int line_counter = 0;
+	/*
+	schedule_t schedule_functinos[] =
+	{
+		schedule1,
+		schedule2
+	};
+	*/
+	if (myfile.is_open()){
+		while (myfile.good()){
+			getline (myfile,line);
+			all_process[line_counter] = string_parser (line);
+			line_counter ++;
+		}
+	}
+	myfile.close();
+	/*
+	schedule_functinos[1] (all_process, line_counter);
+	schedule_functinos[0] (all_process, line_counter);
+	*/
+	for (int i = 0; i< line_counter; i++){
+		cout <<"Process: "<<all_process[i].PID<<" ";
+		cout <<all_process[i].TARQ<<" ";
+		cout <<all_process[i].PRIO<<" ";
+		cout <<all_process[i].TNCPU<<endl;
+		cout <<"CPU/IO Burst: ";
+		for (int j=0; j<all_process[i].TNCPU; j++){
+			cout <<all_process[i].CPU[j]<<" ";
+			if(!(all_process[i].TNCPU == j+1))
+				cout <<all_process[i].IO[j]<<" ";
+		}
+		cout <<endl;
+	}
+	cout <<"Press enter to exit"<<endl;
+	cin.ignore(); 
+    cin.get();
+	cout<<"terminating..."<<endl;
+	return 0;
 }
 
 int string_spliter(string line, int *p_attributes, string delimiter){
@@ -74,48 +129,3 @@ process_t string_parser(string line){//, process_t *out_process, int p_index){
 	return out_process;
 	//out_process[p_index].TNCPU = att[3];
 }
-
-
-
-int main(){ 
-	ifstream myfile;
-	string line;
-	myfile.open("E:/example.txt");
-	//myfile<<"Writing this to a file..... \n";
-	int line_counter = 0;
-
-	schedule_t schedule_functinos[] =
-	{
-		schedule1,
-		schedule2
-	};
-
-	if (myfile.is_open()){
-		while (myfile.good()){
-			getline (myfile,line);
-			all_process[line_counter] = string_parser (line);
-			line_counter ++;
-		}
-	}
-	myfile.close();
-
-	schedule_functinos[1] (all_process, line_counter);
-	schedule_functinos[0] (all_process, line_counter);
-	for (int i = 0; i< line_counter; i++){
-		cout <<"Process: "<<all_process[i].PID<<" ";
-		cout <<all_process[i].TARQ<<" ";
-		cout <<all_process[i].PRIO<<" ";
-		cout <<all_process[i].TNCPU<<endl;
-		cout <<"CPU/IO Burst: ";
-		for (int j=0; j<all_process[i].TNCPU; j++){
-			cout <<all_process[i].CPU[j]<<" "<<all_process[i].IO[j]<<" ";
-		}
-		cout <<endl;
-	}
-	cout <<"Press enter to exit"<<endl;
-	string temp;
-	cin >> temp;
-	cout<<"terminating..."<<endl;
-	return 0;
-}
-
