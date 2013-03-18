377c44fadab9ef7fffc63cec18c3397caa52d10d
ranewu <rane.hr.wu@gmail.com>
2013-03-17 22:46:01 -0700

    Modified processt to store SPB related info

--- Project_2/processt.hpp
+++ Project_2/processt.hpp
@@ -1,3 +1,7 @@
+#ifndef MAX_CPU_BURST
+	#define MAX_CPU_BURST 100
+#endif
+
 #ifndef _PROCESST_HPP_
 #define _PROCESST_HPP_
 
@@ -6,13 +10,15 @@ struct processt {
 	int TARQ;
 	int PRIO;
 	int TNCPU;
-	int CPU[100];
-	int IO[99];
+	int CPU[MAX_CPU_BURST];
+	int IO[MAX_CPU_BURST-1];
 	int current_burst; //indicates the current CPU burst, set to 0 if no burst has occured 
 	int wait_time;
 	int execution_time;
 	int turnaround_time;
 	int age;
+	int average_burst;
+	int last_burst;
 };
 
 #endif
